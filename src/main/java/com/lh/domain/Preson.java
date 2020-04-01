d8b0ccc771c337fbc443e141e828f4b7b0bbece4
liuhuanhuan <liuhuanhuan@com.cn>
2020-04-01 10:33:28 +0800

    新增人类

---
+++ src/main/java/com/lh/domain/Preson.java
@@ -0,0 +1,17 @@
+package com.lh.domain;
+
+public class Preson {
+    private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+    
+}
+	
